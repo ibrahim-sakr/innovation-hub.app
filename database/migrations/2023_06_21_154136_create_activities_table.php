361f29e8f325ac45a0f41d228b797e0a4d56f0cc
Ibrahim Sakr <ebrahimes@gmail.com>
2023-06-25 23:13:56 +0300

    fix table name

--- database/migrations/2023_06_21_154136_create_activities_table.php
+++ database/migrations/2023_06_21_154136_create_activities_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('activities-old', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('icon');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('activities-old');
+        Schema::dropIfExists('activities');
     }
 };
